2918aa16022463deb0e9dc2486b571bfc792b249
zhouqgang <279807387@qq.com>
2019-04-15 13:21:11 +0800

    init

---
+++ src/main/java/com/gaming/bet/BetApplication.java
@@ -0,0 +1,13 @@
+package com.gaming.bet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetApplication.class, args);
+    }
+
+}
